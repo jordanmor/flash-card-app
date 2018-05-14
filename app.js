5f28b185ad500cd5fc16b494a1e5a655ef8e3f7d
Jordan Mor <jordanmor@gmail.com>
2018-05-14 15:23:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get('/cards', (req, res) => {
 });
 
 app.listen(3000, () => {
-	console.log('The application is running on localhost:3000!');
+	console.log('The application is running on localhost:3000');
 });
