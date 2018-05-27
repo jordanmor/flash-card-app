979667cb2c2dfe53916010cbcc2faedcd31fc293
Jordan Mor <jordanmor@gmail.com>
2018-05-27 11:58:03 -0400

    Add link where user can switch to the other side of the card

--- routes/cards.js
+++ routes/cards.js
@@ -8,10 +8,18 @@ router.get('/:id', (req, res) => {
 	const { id } = req.params;
 	const text = cards[id][side];
 	const { hint } = cards[id];
+	const capitalize = function(str) {
+		str = str.toLowerCase();
+		return str.charAt(0).toUpperCase() + str.substr(1);
+	};
 
-	const templateData = { text };
+	const templateData = { text, id, capitalize };
+
 	if (side === 'question') {
 		templateData.hint = hint;
+		templateData.cardOtherSide = 'answer';
+	} else if(side === 'answer'){
+		templateData.cardOtherSide = 'question'
 	}
 	res.render('card', templateData);
 });
